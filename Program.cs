b01be32c8ee193c9fc0160af993e3672f796f7d1
Rudy Gabuco Jr <rudyjr.gabuco@gmail.com>
2024-06-24 13:12:52 -0600

    update

--- Program.cs
+++ Program.cs
@@ -6,5 +6,7 @@
         Console.WriteLine("This will be fun...");
         Console.WriteLine("Yes, this is really fun...");
         Console.WriteLine("Thank you. It was really helpful...");
+        Console.WriteLine("You are welcome...");
+        Console.WriteLine("Have a nice day...");
     }
 }
